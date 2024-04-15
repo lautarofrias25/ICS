6a0f99ae58ffbe5ece1f64db5d5693d1f14077dd
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2024-04-15 02:30:53 -0300

    [Parte 4.2] Utilizacion de vistas genericas

--- Actividad0/polls/views.py
+++ Actividad0/polls/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 from django.db.models import F
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -22,20 +22,20 @@ from .models import Question, Choice
 #def vote(request, question_id):
 #    return HttpResponse("Estas votando en la pregunta %s." % question_id)
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+#def index(request):
+#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#    template = loader.get_template("polls/index.html")
+#    context = {
+#        "latest_question_list": latest_question_list,
+#    }
+#    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("La pregunta no existe.")
-    return render(request, "polls/detail.html", {"question": question})
+#def detail(request, question_id):
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+ #       raise Http404("La pregunta no existe.")
+#    return render(request, "polls/detail.html", {"question": question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -55,8 +55,26 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#   return render(request, "polls/results.html", {"question": question})
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 # Create your views here.
