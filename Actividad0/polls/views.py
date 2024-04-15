df6d30b3976d2607efaa1633f3715b5ff65e91be
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2024-04-14 23:19:21 -0300

    [Parte 3.1] Agregando mas vistas

--- Actividad0/polls/views.py
+++ Actividad0/polls/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hola mundo, estas en el indice de encuestas.")
 
+def detail(request, question_id):
+    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+
+
+def results(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando en la pregunta %s." % question_id)
 # Create your views here.
