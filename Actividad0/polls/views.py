84b39aea5a6a96602483b07376639e4e3d838cf1
Lautaro Frias <lfrias@alu.frlp.utn.edu.ar>
2024-04-15 02:25:07 -0300

    [Parte 4.1] Creando un formulario para poder votar

--- Actividad0/polls/views.py
+++ Actividad0/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.template import loader
-from django.http import Http404
-from .models import Question
+from django.urls import reverse
+from django.db.models import F
+
+from .models import Question, Choice
 
 #def index(request):
 #    return HttpResponse("Hola mundo, estas en el indice de encuestas.")
@@ -11,13 +14,13 @@ from .models import Question
 #    return HttpResponse("Estas viendo la pregunta %s." % question_id)
 
 
-def results(request, question_id):
-    response = "Estas viendo los resultados de la pregunta %s."
-    return HttpResponse(response % question_id)
+#def results(request, question_id):
+#    response = "Estas viendo los resultados de la pregunta %s."
+#    return HttpResponse(response % question_id)
 
 
-def vote(request, question_id):
-    return HttpResponse("Estas votando en la pregunta %s." % question_id)
+#def vote(request, question_id):
+#    return HttpResponse("Estas votando en la pregunta %s." % question_id)
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -33,5 +36,27 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("La pregunta no existe.")
     return render(request, "polls/detail.html", {"question": question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "No seleccionaste una opcion.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 # Create your views here.
